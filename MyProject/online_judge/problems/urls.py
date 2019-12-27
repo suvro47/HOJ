7377358db9000af1d73d7c6f35f15fa3845a930c
Sohel Suvro <suvro047@gmail.com>
2019-12-27 15:36:24 +0600

    v2.0

--- MyProject/online_judge/problems/urls.py
+++ MyProject/online_judge/problems/urls.py
@@ -3,6 +3,5 @@ from .import views
 
 
 urlpatterns = [
-    path('problem-list/', views.problem_list, name='problem-list'),
-    path('single-problem/<problem_id>/', views.single_problem, name='single-problem'),
+
 ]
