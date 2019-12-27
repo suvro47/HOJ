a1925b9ee09a9ac797d83c68826af332770120af
Sohel Suvro <suvro047@gmail.com>
2019-12-27 15:37:51 +0600

    v1.0

--- MyProject/online_judge/online_judge/settings.py
+++ MyProject/online_judge/online_judge/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'accounts',
     'problems',
     'ckeditor',
-    'submissions',
 ]
 
 MIDDLEWARE = [
--- MyProject/online_judge/problems/urls.py
+++ MyProject/online_judge/problems/urls.py
@@ -3,5 +3,6 @@ from .import views
 
 
 urlpatterns = [
-
+    path('problem-list/', views.problem_list, name='problem-list'),
+    path('single-problem/<problem_id>/', views.single_problem, name='single-problem'),
 ]
