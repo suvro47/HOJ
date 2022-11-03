5e68d23e1687323406f3dd8f1fc634d4bd6b16d1
suvro47 <sohel.cse.hstu@gmail.com>
2022-11-03 18:37:51 +0600

    unnecessary folder delted

--- main_conf/problem/forms.py
+++
@@ -1,9 +0,0 @@
-from ckeditor.fields import RichTextFormField
-
-from django import forms
-from submission.models import Submission
-
-class SubmitForm(forms.ModelForm):
-    class Meta:
-        model = Submission
-        fields = ('language', 'code')
--- main_conf/submission/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from submission.models import Submission
-
-admin.site.register(Submission)
-
-
-
